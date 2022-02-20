227fff1c3fd5de7fa3072e5c9bcdc7e7b04a670a
SamsonShuttle <samsonshuttle@sky.com>
2022-02-20 10:55:29 +0000

    Initial Commit

---
+++ L2 LearningGH/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  L2 LearningGH
+//
+//  Created by Samson Shuttle on 20/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ L2 LearningGH/L2_LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  L2_LearningGHApp.swift
+//  L2 LearningGH
+//
+//  Created by Samson Shuttle on 20/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct L2_LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
